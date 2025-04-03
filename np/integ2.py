69274f300e43b3b3e70846940bde3420751dc74e
Prasenjit Saha <psaha@physik.uzh.ch>
2025-04-03 14:07:34 +0200

    edits

---
+++ np/integ2.py
@@ -0,0 +1,83 @@
+import numpy as np
+import math
+import matplotlib.pyplot as pl
+from astropy.time import Time
+
+data = np.loadtxt('inic.txt')
+
+sGM, eGM, mGM = data[:,0]
+ex,ey,ez = data[1,1:4]
+mx,my,mz = data[2,1:4]
+evx,evy,evz = data[1,4:]
+mvx,mvy,mvz = data[2,4:]
+
+
+def inner(ax,ay,az,bx,by,bz):
+    num = ax*bx + ay*by + az*bz
+    dena = ax*ax + ay*ay + az*az
+    denb = bx*bx + by*by + bz*bz
+    return num/(dena*denb)**(1/2)
+
+
+def pred(dy,c):
+    date = Time(dy+2460000,format='jd')
+    if c[1] < -.9998 and c[1] == min(c):
+        print('maybe lunar',date.iso)
+    if c[2] > .9998 and c[1] == max(c):
+        print('maybe solar',date.iso)
+        
+def drift(dt):
+    global ex,ey,ez,mx,my,mz,evx,evy,evz,mvx,mvy,mvz
+    ex += evx * dt/2
+    ey += evy * dt/2
+    ez += evz * dt/2
+    mx += mvx * dt/2
+    my += mvy * dt/2
+    mz += mvz * dt/2
+    
+def accel(GM,x,y,z):
+    f = -GM/(x*x + y*y + z*z)**(3/2)
+    return x*f, y*f, z*f
+
+def kick(dt):
+    global ex,ey,ez,mx,my,mz,evx,evy,evz,mvx,mvy,mvz
+    ax, ay, az = accel(sGM,ex,ey,ez)
+    evx, evy, evz = evx+ax*dt, evy+ay*dt, evz+az*dt
+    ax, ay, az = accel(mGM,ex-mx,ey-my,ez-mz)
+    evx, evy, evz = evx+ax*dt, evy+ay*dt, evz+az*dt
+    ax, ay, az = accel(sGM,mx,my,mz)
+    mvx, mvy, mvz = mvx+ax*dt, mvy+ay*dt, mvz+az*dt
+    ax, ay, az = accel(eGM,mx-ex,my-ey,mz-ez)
+    mvx, mvy, mvz = mvx+ax*dt, mvy+ay*dt, mvz+az*dt
+    
+t = 0
+mjd = []
+dot = []
+
+dt = 1200
+day = 86400
+
+yr2026 = 1405 * day
+yr2100 = 28433 * day
+
+while t < yr2026:
+    # integration step
+    drift(dt)
+    kick(dt)
+    drift(dt)
+    t += dt
+    # save and check for eclipses
+    mjd.append(t/day)
+    d = inner(ex-mx,ey-my,ez-mz,ex,ey,ez)
+    dot.append(d)
+    if len(mjd) > 3:
+        pred(mjd[-2],dot[-3:])
+    
+ang = [math.acos(x)*180/np.pi for x in dot]
+
+pl.plot(mjd,ang)
+pl.xlabel('JD - 2460000')
+pl.ylabel('Sun-Moon angle in degrees')
+
+pl.show()
+
